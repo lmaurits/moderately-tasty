475f184fc153e6b72d37874ae8eaecdf75c0d17f
Luke Maurits <luke@maurits.id.au>
2013-03-31 12:32:12 -0700

    Use function wrapper to control access to db.

--- modtasty.py
+++ modtasty.py
@@ -1,4 +1,5 @@
 import collections
+import functools
 import sqlite3
 import urlparse
 import time
@@ -27,37 +28,52 @@ class Link():
         else:
             return "Untitled (%s)" % self.url
 
+def db_access(f):
+    @functools.wraps(f)
+    def decorated(self, *args, **kwargs):
+        already_open = self.db_open
+        if not self.db_open:
+            self.con = sqlite3.connect("modtasty.db")
+            self.cur = self.con.cursor()
+            self.db_open = True
+        retval = f(self, *args, **kwargs)
+        if not already_open:
+            self.cur.close()
+            self.con.commit()
+            self.con.close()
+            self.db_open = False
+        return retval
+    return decorated
+
 class ModTasty():
 
     def __init__(self):
 
+        self.db_open = False
         self.initialise_db()
 
+    @db_access
     def initialise_db(self):
-        con = sqlite3.connect("modtasty.db")
-        cur = con.cursor()
-        cur.execute("""CREATE TABLE IF NOT EXISTS links (
+
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS links (
                 id INTEGER PRIMARY KEY ASC,
                 title TEXT,
                 url TEXT,
                 created INTEGER
                 )""")
 
-        cur.execute("""CREATE TABLE IF NOT EXISTS tags (
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS tags (
                 id INTEGER PRIMARY KEY ASC,
                 name TEXT
                 )""")
 
-        cur.execute("""CREATE TABLE IF NOT EXISTS link_tag_connections (
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS link_tag_connections (
                 link_id INTEGER,
                 tag_id INTEGER,
                 FOREIGN KEY(link_id) REFERENCES links(id),
                 FOREIGN KEY(tag_id) REFERENCES tags(id)
                 )""")
 
-        con.commit()
-        cur.close()
-        con.close()
 
     def make_link_from_table_row(self, row):
         link = Link()
@@ -74,91 +90,69 @@ class ModTasty():
         link.title = s.title.string
         return link
 
+    @db_access
     def save_link(self, link):
-        con = sqlite3.connect("modtasty.db")
-        cur = con.cursor()
-
         if not link.created:
             link.created = time.time()
         if link.id:
-            cur.execute("""UPDATE links SET title=?, url=? WHERE id=?""", (link.title, link.url, link.id))
+            self.cur.execute("""UPDATE links SET title=?, url=? WHERE id=?""", (link.title, link.url, link.id))
         else:
-            cur.execute("""INSERT INTO links (title, url, created) VALUES (?, ?, ?)""", (link.title, link.url, link.created))
-            cur.execute("""SELECT id FROM links WHERE url=?""", (link.url,))
-            link.id = cur.fetchone()[0]
+            self.cur.execute("""INSERT INTO links (title, url, created) VALUES (?, ?, ?)""", (link.title, link.url, link.created))
+            self.cur.execute("""SELECT id FROM links WHERE url=?""", (link.url,))
+            link.id = self.cur.fetchone()[0]
 
-        cur.execute("""DELETE from link_tag_connections WHERE link_id=?""", (link.id,))
+        self.cur.execute("""DELETE from link_tag_connections WHERE link_id=?""", (link.id,))
         for tag in link.tags:
-            cur.execute("""SELECT id FROM tags WHERE name=?""", (tag,))
-            tag_id = cur.fetchall()
+            self.cur.execute("""SELECT id FROM tags WHERE name=?""", (tag,))
+            tag_id = self.cur.fetchall()
             if tag_id:
                 tag_id = tag_id[0][0]
-                cur.execute("""INSERT INTO link_tag_connections VALUES (?, ?)""", (link.id, tag_id))
+                self.cur.execute("""INSERT INTO link_tag_connections VALUES (?, ?)""", (link.id, tag_id))
             else:
-                cur.execute("""INSERT INTO tags (name) VALUES (?)""", (tag, ))
-                cur.execute("""SELECT id FROM tags WHERE name=?""", (tag, ))
-                tag_id = cur.fetchone()[0]
-                cur.execute("""SELECT id FROM tags WHERE name=?""", (tag, ))
-                cur.execute("""INSERT INTO link_tag_connections VALUES (?, ?)""", (link.id, tag_id))
+                self.cur.execute("""INSERT INTO tags (name) VALUES (?)""", (tag, ))
+                self.cur.execute("""SELECT id FROM tags WHERE name=?""", (tag, ))
+                tag_id = self.cur.fetchone()[0]
+                self.cur.execute("""SELECT id FROM tags WHERE name=?""", (tag, ))
+                self.cur.execute("""INSERT INTO link_tag_connections VALUES (?, ?)""", (link.id, tag_id))
 
-        con.commit()
-        cur.close()
-        con.close()
-
+    @db_access
     def get_link_by_id(self, id):
-        con = sqlite3.connect("modtasty.db")
-        cur = con.cursor()
-        cur.execute("""SELECT * FROM links WHERE id=?""", (id, ))
-        link = self.make_link_from_table_row(cur.fetchone())
-        cur.execute("""SELECT tag_id FROM link_tag_connections WHERE link_id=?""", (id, ))
-        for tag_id in cur.fetchall():
+        self.cur.execute("""SELECT * FROM links WHERE id=?""", (id, ))
+        link = self.make_link_from_table_row(self.cur.fetchone())
+        self.cur.execute("""SELECT tag_id FROM link_tag_connections WHERE link_id=?""", (id, ))
+        for tag_id in self.cur.fetchall():
             tag_id = tag_id[0]
-            cur.execute("""SELECT name FROM tags WHERE id=?""", (tag_id, ))
-            link.tags.append(cur.fetchone()[0])
+            self.cur.execute("""SELECT name FROM tags WHERE id=?""", (tag_id, ))
+            link.tags.append(self.cur.fetchone()[0])
             link.tags.sort()
-        cur.close()
-        con.close()
         return link
 
+    @db_access
     def delete_link_by_id(self, id):
-        con = sqlite3.connect("modtasty.db")
-        cur = con.cursor()
-        cur.execute("""DELETE FROM links WHERE id=?""", (id, ))
-        cur.execute("""DELETE FROM link_tag_connections WHERE link_id=?""", (id, ))
+        self.cur.execute("""DELETE FROM links WHERE id=?""", (id, ))
+        self.cur.execute("""DELETE FROM link_tag_connections WHERE link_id=?""", (id, ))
         # TODO check if we just deleted the last link for any tag and if so del tag
-        con.commit()
-        cur.close()
-        con.close()
 
+    @db_access
     def get_latest_links(self):
-        con = sqlite3.connect("modtasty.db")
-        cur = con.cursor()
-        cur.execute("""SELECT id FROM links ORDER BY created DESC LIMIT 20""")
-        links = [self.get_link_by_id(id[0]) for id in cur.fetchall()]
-        cur.close()
-        con.close()
+        self.cur.execute("""SELECT id FROM links ORDER BY created DESC LIMIT 20""")
+        links = [self.get_link_by_id(id[0]) for id in self.cur.fetchall()]
         return links
 
+    @db_access
     def get_all_tags_and_counts(self):
-        con = sqlite3.connect("modtasty.db")
-        cur = con.cursor()
-        cur.execute("""SELECT tags.name, COUNT(tags.id)  FROM tags INNER JOIN link_tag_connections ON tags.id = link_tag_connections.tag_id GROUP BY tags.id ORDER BY COUNT(tags.id) DESC""")
-        tc = cur.fetchall()
+        self.cur.execute("""SELECT tags.name, COUNT(tags.id)  FROM tags INNER JOIN link_tag_connections ON tags.id = link_tag_connections.tag_id GROUP BY tags.id ORDER BY COUNT(tags.id) DESC""")
+        tc = self.cur.fetchall()
         tags = [t[0] for t in tc]
         counts = [t[1] for t in tc]
-        cur.close()
-        con.close()
         return tags, counts
        
+    @db_access
     def get_links_by_tag_name(self, tag_name):
-        con = sqlite3.connect("modtasty.db")
-        cur = con.cursor()
-        cur.execute("""SELECT id FROM tags WHERE name=?""", (tag_name,))
-        tag_id = cur.fetchall()
+        self.cur.execute("""SELECT id FROM tags WHERE name=?""", (tag_name,))
+        tag_id = self.cur.fetchall()
         if not tag_id:
             return []
-        cur.execute("""SELECT links.id FROM links INNER JOIN link_tag_connections ON links.id = link_tag_connections.link_id WHERE link_tag_connections.tag_id=? ORDER BY links.created DESC""", (tag_id[0][0],))
-        links = [self.get_link_by_id(id[0]) for id in cur.fetchall()]
-        cur.close()
-        con.close()
+        self.cur.execute("""SELECT links.id FROM links INNER JOIN link_tag_connections ON links.id = link_tag_connections.link_id WHERE link_tag_connections.tag_id=? ORDER BY links.created DESC""", (tag_id[0][0],))
+        links = [self.get_link_by_id(id[0]) for id in self.cur.fetchall()]
         return links
